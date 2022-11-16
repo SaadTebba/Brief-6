1356b511e7adca94b113e319b3f3d74b1886d77c
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2022-11-16 23:08:33 +0100

    -----

--- script.js
+++ script.js
@@ -3,9 +3,24 @@
 // let email = document.getElementById('Email');
 // let club = document.getElementById('Club(s)');
 
-let télephone = "0656923250";
-let télephoneRe = /\d{3}[ -]?\d{3}[ -]?\d{4}/;
-console.log(télephone.match(télephoneRe))
+// let télephone = "0656923250";
+// let télephoneRe = /\d{3}[ -]?\d{3}[ -]?\d{4}/;
+// console.log(télephone.match(télephoneRe))
 
 // if (télephoneRe) ;
 // else console.log(télephone + " number is invalid")
+
+
+// let télephoneReg = /[^a-zA-Z]{10,10}/g;
+
+if (télephoneReg = /[^a-zA-Z]{10,10}/g) {
+  let télephoneReg = document.getElementById("Télephone");
+  console.log("Number is valide");
+} else {
+  let output = (document.getElementById("outputtest").innerHTML =
+    "Number is invalideeeeeee");
+}
+
+function testingwsf() {
+  console.log("hola test");
+}
