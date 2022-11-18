fade4d4bb123d3c253dd368cab4f2729fb68a4e5
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2022-11-18 17:30:33 +0100

    email done

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-// let nom = document.getElementById('Nom');
-// let prénom = document.getElementById('Prénom');
-// let email = document.getElementById('Email');
-// let club = document.getElementById('Club(s)');
-
-// let télephone = "0656923250";
-// let télephoneRe = /\d{3}[ -]?\d{3}[ -]?\d{4}/;
-// console.log(télephone.match(télephoneRe))
-
-// if (télephoneRe) ;
-// else console.log(télephone + " number is invalid")
+// let gmailValidation = /([A-Za-z0-9]\w+(\.?)\w+(\.?)\w+)@(gmail|yahoo|ofppt)\.(com|ma)/;
+// let phoneNumberValidation = /(05|06|07)([0-9]\d+)/;
+// let nomPrenomValidation = /[a-zA-Z]{3,30}/;
 
 
-// let télephoneReg = /[^a-zA-Z]{10,10}/g;
+{
 
-if (télephoneReg = /[^a-zA-Z]{10,10}/g) {
-  let télephoneReg = document.getElementById("Télephone");
-  console.log("Number is valide");
-}
-else {
-  let output = (document.getElementById("outputtest").innerHTML = "Number is invalideeeeeee");
-}
+  let email = document.getElementById("Email").value;
+  let emailValidation = /([A-Za-z0-9]\w+(\.?)\w+(\.?)\w+)@(gmail|yahoo|ofppt)\.(com|ma)/g;
 
-function testingwsf() {
-  console.log("hola test");
+  if (emailValidation.test(email)) {
+    document.getElementById("Email").style.borderColor = "";
+    document.getElementById("outputemail").innerHTML = "Email is valid"
+    return true;
+  }
+  else {
+    document.getElementById("Email").style.borderColor = "#FF0000";
+    document.getElementById("outputemail").innerHTML = "Email is not valid"
+    return false
+  }
+
 }
 
+{
+  let nom = document.getElementById('Nom').value;
 
-
-
-
-
-
-
-
-
-
-
-// ([A-Za-z0-9]\w+(\.?)\w+(\.?)\w+)@(gmail|yahoo|ofppt)\.(com|ma) // gmaillll
-// (05|06|07)([0-9]\d+) // number - but without spaces or + or anything
+}
